f28759fe4d50bee01c3c444fa4e66d7d8e397109
Nik <ice_fire50@mail.ru>
2023-07-12 17:09:14 +0300

    implementation search and completion

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
 
+from contextlib import nullcontext
 from datetime import datetime
 import os
 
@@ -19,19 +20,70 @@ def id(line):                                                                   
    mass = line.split(";")
    return int (mass[0])
 
+def time_code():                                                                # Формирует из времени в id для поиска строки
+    now=datetime.now()
+    year = input("Введите полный год: ")
+    if year == "": year=now.strftime("%Y")
+    month = input("Введите месяц двухзначным числом: ")
+    if month == "": month="01"
+    day = input("Введите день двухзначным числом: ")
+    if day == "": day="01"
+    hour = input("Введите час двухзначным числом по 24h: ")
+    if hour == "": hour="00"
+    return int (year+month+day+hour+"0000")
+
 def show():                                                                     # Печать всей базы
     with open(FILE_PATH,'r',encoding='utf-8') as base:
         print(base.read())
 
 def add():                                                                      # Добавление новой записи в файл
     now=datetime.now()                                                          # текущее время
-    new_id=now.strftime("%Y%m%d%H%M%S")                                         # 
+    new_id=now.strftime("%Y%m%d%H%M%S")                                         # ID формируется из времени создания или редактирования
     date=now.strftime("%d.%m.%Y %H:%M")                                         # Запись текущего времени в строку
     name=input("Введите название заметки: ")
     notes=input("Напишите заметку: ")
 
     with open(FILE_PATH,'a',encoding='utf-8') as base:
         base.write(new_id+';'+date+';'+name+';'+notes+'\n')
+
+def search():
+    print("[1] Вывести все сообщения ДО введенного времени")
+    print("[2] Вывести все сообщения ПОСЛЕ введенного времени")
+    print("[3] Вывести все сообщения в промежутке введенного времени")
+    incom =int(input("Выберите режим поиска: "))
+    if incom == 1:
+        id_find = time_code()
+        with open(FILE_PATH,'r',encoding='utf-8') as base:
+            while True:
+                info=base.readline()
+                if info:
+                    if id(info)<=id_find:
+                        print(info)
+                else:
+                    break
+    elif incom ==2:
+        id_find = time_code()
+        with open(FILE_PATH,'r',encoding='utf-8') as base:
+            while True:
+                info=base.readline()
+                if info:
+                    if id(info)>=id_find:
+                        print(info)
+                else:
+                    break
+    elif incom ==3:
+        print("C какого времени искать")
+        id_find_star = time_code()
+        print("Каким временем закончить")
+        id_find_end =time_code()
+        with open(FILE_PATH,'r',encoding='utf-8') as base:
+            while True:
+                info=base.readline()
+                if info:
+                    if id_find_star<id(info) <=id_find_end:
+                        print(info)
+                else:
+                    break
 
 def edit():                                                                   # Замена выбранной строки
     index=int(input("Введите id строки для замены: "))
